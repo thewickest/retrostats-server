b1ba0801c3d794dd51e71a5abbe3778fe8978820
thewickest <graubuntu@gmail.es>
2024-10-21 13:02:48 +0200

    Missing class in switcher

--- switcher.py
+++ switcher.py
@@ -3,5 +3,6 @@ from roms.mameLibretro import mame2003
 class Switcher(object):
     def hiToText(self, textScorePath, byteScorePath, gameName):
         methodName = f'{gameName}Parser'
-        if hasattr(self, methodName) and callable(method := getattr(self, methodName)):
+        # TODO this is better but should be totaly dynamic
+        if hasattr(mame2003, methodName) and callable(method := getattr(mame2003, methodName)):
             return method(textScorePath, byteScorePath)
